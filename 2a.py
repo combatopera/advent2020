f737ff48de3ff2f8620dfb1ecd5f0756e286ad17
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-12-02 14:34:21 +0000

    yyup

---
+++ 2a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+import re
+
+pattern = re.compile('([0-9]+)-([0-9]+) (.): (.+)')
+
+def main():
+    v = [pattern.fullmatch(l).groups() for l in Path('input', '2').read_text().splitlines()]
+    valid = 0
+    for m, n, c, p in v:
+        m, n = map(int, [m, n])
+        k = p.count(c)
+        valid += m <= k and k <= n
+    print(valid)
+
+if '__main__' == __name__:
+    main()
