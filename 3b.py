1606202c5d3a560c9e2591699b1f1086669df79c
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-12-03 12:35:48 +0000

    yup

---
+++ 3b.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from functools import reduce
+from pathlib import Path
+import operator
+
+slopes = (
+    [1, 1],
+    [3, 1],
+    [5, 1],
+    [7, 1],
+    [1, 2],
+)
+
+class Map:
+
+    def __init__(self, rows):
+        self.w = len(rows[0])
+        self.rows = rows
+
+    def tree(self, x, y):
+        return '#' == self.rows[y][x % self.w]
+
+    def trees(self, slope):
+        xy = [0, 0]
+        trees = 0
+        try:
+            while True:
+                for i in range(2):
+                    xy[i] += slope[i]
+                trees += self.tree(*xy)
+        except IndexError:
+            return trees
+
+def main():
+    m = Map(Path('input', '3').read_text().splitlines())
+    print(reduce(operator.mul, map(m.trees, slopes)))
+
+if '__main__' == __name__:
+    main()
