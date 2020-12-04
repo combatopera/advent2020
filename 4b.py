d5e03a255358b55946e825a24dac8cbf56cbb78e
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-12-04 17:36:20 +0000

    cool

---
+++ 4b.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+import re
+
+class Pattern:
+
+    def __init__(self, regex):
+        self.pattern = re.compile(regex)
+
+    def __call__(self, text):
+        return self.pattern.fullmatch(text)
+
+class Int:
+
+    intpattern = Pattern('[0-9]+')
+
+    def __init__(self, min, max):
+        self.r = range(min, max + 1)
+
+    def __call__(self, text):
+        return self.intpattern(text) and int(text) in self.r
+
+class And:
+
+    def __init__(self, *rules):
+        self.rules = rules
+
+    def __call__(self, text):
+        return all(r(text) for r in self.rules)
+
+class Switch:
+
+    def __init__(self, key, value, rules):
+        self.key = key
+        self.value = value
+        self.rules = rules
+
+    def __call__(self, text):
+        k = self.key(text)
+        if k in self.rules:
+            return self.rules[k](self.value(text))
+
+yearpattern = Pattern('[0-9]{4}')
+rules = dict(
+    byr = And(yearpattern, Int(1920, 2002)),
+    iyr = And(yearpattern, Int(2010, 2020)),
+    eyr = And(yearpattern, Int(2020, 2030)),
+    hgt = Switch(lambda s: s[-2:], lambda s: s[:-2], {'cm': Int(150, 193), 'in': Int(59, 76)}),
+    hcl = Pattern('#[0-9a-f]{6}'),
+    ecl = lambda s: s in {'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'},
+    pid = Pattern('[0-9]{9}'),
+)
+
+def _read():
+    with Path('input', '4').open() as f:
+        for line in f:
+            yield line.rstrip()
+    yield ''
+
+def main():
+    valid = 0
+    p = {}
+    for l in _read():
+        if l:
+            p.update(e.split(':') for e in l.split(' '))
+        else:
+            valid += all(f in p and rule(p[f]) for f, rule in rules.items())
+            p = {}
+    print(valid)
+
+if '__main__' == __name__:
+    main()
