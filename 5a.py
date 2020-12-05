5957483533d29b8d628c85607e4fa1a100da68ba
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-12-05 09:59:54 +0000

    just binary innit

--- 5a.py
+++ 5a.py
@@ -2,13 +2,13 @@
 
 from pathlib import Path
 
+xform = dict(F = 0, B = 1, L = 0, R = 1)
+
 def main():
     def g():
         with Path('input', '5').open() as f:
-            for line in f:
-                row = eval(f"0b{line[:7].replace('F', '0').replace('B', '1')}")
-                col = eval(f"0b{line[7:10].replace('L', '0').replace('R', '1')}")
-                yield row * 8 + col
+            for l in f:
+                yield sum(2 ** i * xform[c] for i, c in enumerate(l[9::-1]))
     print(max(g()))
 
 if '__main__' == __name__:
