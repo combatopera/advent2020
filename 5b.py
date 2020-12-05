7eab19249734bb36c9e6c982992693bbc06117a6
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-12-05 10:19:15 +0000

    undup

--- 5b.py
+++ 5b.py
@@ -6,13 +6,13 @@ xform = dict(F = 0, B = 1, L = 0, R = 1)
 size = 10
 
 def main():
-    n = 2 ** size
-    seats = set(range(n))
+    space = range(2 ** size)
+    seats = set(space)
     with Path('input', '5').open() as f:
         for l in f:
             seats.remove(sum(2 ** i * xform[c] for i, c in enumerate(l[size - 1::-1])))
     try:
-        for s in range(n):
+        for s in space:
             seats.remove(s)
     except KeyError:
         print(min(seats))
