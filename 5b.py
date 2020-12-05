db0208507919bc934bf6b6487fef611b5f0fa3c5
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-12-05 10:27:00 +0000

    efficienter

--- 5b.py
+++ 5b.py
@@ -4,13 +4,15 @@ from pathlib import Path
 
 xform = dict(F = 0, B = 1, L = 0, R = 1)
 size = 10
+factors = [2 ** i for i in range(size)]
+factors.reverse()
 
 def main():
     space = range(2 ** size)
     seats = set(space)
     with Path('input', '5').open() as f:
         for l in f:
-            seats.remove(sum(2 ** i * xform[c] for i, c in enumerate(l[size - 1::-1])))
+            seats.remove(sum(f * xform[l[i]] for i, f in enumerate(factors)))
     try:
         for s in space:
             seats.remove(s)
