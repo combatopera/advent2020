5f03f5fa4b2ca45785c89034b53defbcb62cc32b
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-12-09 18:22:08 +0000

    uhu

--- 9a.py
+++ 9a.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 width = 25
 
-class Matrix:
+class Triangle:
 
     class Pair:
 
@@ -18,26 +18,26 @@ class Matrix:
     cursor = 0
 
     def __init__(self, size):
-        self.rows = [[self.Pair() for _ in range(size)] for _ in range(size)]
+        self.rows = [[self.Pair() for _ in range(r)] for r in range(size)]
 
     def accept(self, x):
         return any(p.sum == x for r in self.rows for p in r)
 
     def update(self, x):
-        for p in self.rows[self.cursor]:
+        for p in self.rows[self.cursor][:self.cursor]:
             p.update(0, x)
-        for r in self.rows:
+        for r in self.rows[self.cursor + 1:]:
             r[self.cursor].update(1, x)
         self.cursor = (self.cursor + 1) % len(self.rows)
 
 def main():
     with Path('input', '9').open() as f:
         values = [int(l) for l in f]
-    m = Matrix(width)
+    t = Triangle(width)
     for i, x in enumerate(values):
-        if i >= width and not m.accept(x):
+        if i >= width and not t.accept(x):
             break
-        m.update(x)
+        t.update(x)
     print(x)
 
 if '__main__' == __name__:
