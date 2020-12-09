51b40e556eded91eb1fbe6a0fafeaf80ba9cfd63
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-12-09 18:16:59 +0000

    interesting

--- 9a.py
+++ 9a.py
@@ -4,17 +4,41 @@ from pathlib import Path
 
 width = 25
 
+class Matrix:
+
+    class Pair:
+
+        def __init__(self):
+            self.pair = [0, 0]
+
+        def update(self, i, x):
+            self.pair[i] = x
+            self.sum = sum(self.pair) if self.pair[0] != self.pair[1] else None
+
+    cursor = 0
+
+    def __init__(self, size):
+        self.rows = [[self.Pair() for _ in range(size)] for _ in range(size)]
+
+    def accept(self, x):
+        return any(p.sum == x for r in self.rows for p in r)
+
+    def update(self, x):
+        for p in self.rows[self.cursor]:
+            p.update(0, x)
+        for r in self.rows:
+            r[self.cursor].update(1, x)
+        self.cursor = (self.cursor + 1) % len(self.rows)
+
 def main():
     with Path('input', '9').open() as f:
         values = [int(l) for l in f]
-    ring = values[:width]
-    cursor = 0
-    for value in values[width:]:
-        if not any(x != y and x + y == value for x in ring for y in ring):
+    m = Matrix(width)
+    for i, x in enumerate(values):
+        if i >= width and not m.accept(x):
             break
-        ring[cursor] = value
-        cursor = (cursor + 1) % width
-    print(value)
+        m.update(x)
+    print(x)
 
 if '__main__' == __name__:
     main()
