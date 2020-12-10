9ae1e60af0ddff7721af753ca90672f0a90c47e3
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-12-10 21:04:53 +0000

    refactoring

--- 10a.py
+++ 10a.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 
-from collections import defaultdict
+from itertools import islice
 from pathlib import Path
+
+def differentiate(v):
+    return [y - x for x, y in zip(v, islice(v, 1, None))]
 
 def main():
     with Path('input', '10').open() as f:
-        adapters = [int(l) for l in f]
-    adapters.sort()
-    adapters.append(max(adapters) + 3)
-    counts = defaultdict(int)
-    joltage = 0
-    for a in adapters:
-        counts[a - joltage] += 1
-        joltage = a
-    assert counts.keys() <= {1, 2, 3}
-    print(counts[1] * counts[3])
+        joltages = [int(l) for l in f]
+    joltages.append(max(joltages) + 3)
+    joltages.append(0)
+    joltages.sort()
+    diffs = differentiate(joltages)
+    print(diffs.count(1) * diffs.count(3))
 
 if '__main__' == __name__:
     main()
