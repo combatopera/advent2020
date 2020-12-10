0ea38c6b93fb745fc476c1f8e60bc9716ce78157
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-12-10 21:13:56 +0000

    refactoring

--- 10b.py
+++ 10b.py
@@ -1,26 +1,23 @@
 #!/usr/bin/env python3
 
+from adventlib import differentiate
 from functools import reduce
-from itertools import islice
 from pathlib import Path
 import operator, re
 
-def arrangements(runsize):
+def factor(runsize):
     return min(7, 2 ** (runsize - 1))
 
 def main():
-    joltages = [0]
     with Path('input', '10').open() as f:
-        joltages.extend(int(l) for l in f)
+        joltages = [int(l) for l in f]
+    joltages.append(max(joltages) + 3)
+    joltages.append(0)
     joltages.sort()
-    print(joltages)
-    diffs = ''.join(str(j1 - j0) for j0, j1 in zip(joltages, islice(joltages, 1, None)))
-    print(diffs)
-    runs = [len(run) for run in re.split('3+', diffs)]
-    print(runs)
-    factors = [arrangements(rs) for rs in runs]
-    print(factors)
-    print(reduce(operator.mul, factors))
+    diffs = differentiate(joltages)
+    assert 2 not in diffs
+    runs = [len(run) for run in re.findall('1+', ''.join(map(str, diffs)))]
+    print(reduce(operator.mul, map(factor, runs)))
 
 if '__main__' == __name__:
     main()
