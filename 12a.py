aa35816058f886f93ea1e8130e09eb1fb65c6c83
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-12-12 10:26:59 +0000

    works

---
+++ 12a.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+
+class Turtle:
+
+    east = 0
+    north = 0
+    facing = 0
+    facings = ((1, 0), (0, 1), (-1, 0), (0, -1))
+
+    def N(self, number):
+        self.north += number
+
+    def S(self, number):
+        self.north -= number
+
+    def E(self, number):
+        self.east += number
+
+    def W(self, number):
+        self.east -= number
+
+    def L(self, degrees):
+        self.facing = (self.facing + degrees // 90) % len(self.facings)
+
+    def R(self, degrees):
+        self.L(-degrees)
+
+    def F(self, number):
+        facing = self.facings[self.facing]
+        self.E(number * facing[0])
+        self.N(number * facing[1])
+
+    def manhattan(self):
+        return abs(self.east) + abs(self.north)
+
+def main():
+    t = Turtle()
+    with Path('input', '12').open() as f:
+        for l in f:
+            getattr(t, l[0])(int(l[1:]))
+    print(t.manhattan())
+
+if '__main__' == __name__:
+    main()
