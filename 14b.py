a38e105862d565143dd5eccf97aa473ae49f6875
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-12-14 13:25:49 +0000

    well that took way longer than it should have done

---
+++ 14b.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from itertools import chain, combinations
+from pathlib import Path
+import re
+
+pattern = re.compile(r'(?:mask|mem\[([0-9]+)\]) = (.+)')
+
+def main():
+    memory = {}
+    with Path('input', '14').open() as f:
+        for l in f:
+            addrstr, rhs = pattern.fullmatch(l.rstrip()).groups()
+            if addrstr is None:
+                ones = int(rhs.translate(str.maketrans('X', '0')), 2)
+                addrmask = int(rhs.translate(str.maketrans('X10', '001')), 2)
+                floats = [2 ** i for i, c in enumerate(reversed(rhs)) if 'X' == c]
+                #print(rhs, f"{ones:b}", f"{addrmask:b}", floats)
+            else:
+                addr0 = int(addrstr) & addrmask | ones
+                #print(f"{int(addrstr):b} {addrmask:b} {ones:b} {addr0:b}")
+                val = int(rhs)
+                # Adapt powerset recipe:
+                for size in range(len(floats) + 1):
+                    for n in map(sum, combinations(floats, size)):
+                        memory[addr0 | n] = val
+    print(sum(memory.values()))
+
+if '__main__' == __name__:
+    main()
