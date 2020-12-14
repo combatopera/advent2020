90b59c88763f8d4e47b36d43f34a50eaca23143f
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-12-14 12:54:35 +0000

    yay

---
+++ 14a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+import re
+
+pattern = re.compile(r'(?:mask|mem\[([0-9]+)\]) = (.+)')
+
+def main():
+    memory = {}
+    with Path('input', '14').open() as f:
+        for l in f:
+            address, rhs = pattern.fullmatch(l.rstrip()).groups()
+            if address is None:
+                ones = int(rhs.replace('X', '0'), 2)
+                valmask = int(rhs.translate(str.maketrans('X1', '10')), 2)
+            else:
+                memory[int(address)] = int(rhs) & valmask | ones
+    print(sum(memory.values()))
+
+if '__main__' == __name__:
+    main()
