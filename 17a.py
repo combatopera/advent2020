b2d111a6b96a180010e3329961e9c668637b91c6
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-12-17 09:40:57 +0000

    yay

---
+++ 17a.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+
+kernel = [(x, y, z) for r in [range(-1, 2)] for x in r for y in r for z in r if x or y or z]
+
+def vadd(u, v):
+    return tuple(u[i]+v[i] for i in range(3))
+
+def main():
+    active = set()
+    with Path('input', '17').open() as f:
+        for y, l in enumerate(f):
+            for x, c in enumerate(l.rstrip()):
+                if '#' == c:
+                    active.add((x, y, 0))
+    for _ in range(6):
+        newactive = set()
+        candidates = set()
+        for cube in active:
+            if sum(1 for k in kernel if vadd(cube, k) in active) in {2, 3}:
+                newactive.add(cube)
+            candidates.update(vadd(cube, k) for k in kernel)
+        for cube in candidates:
+            if sum(1 for k in kernel if vadd(cube, k) in active) == 3:
+                newactive.add(cube)
+        active = newactive
+    print(len(active))
+
+if '__main__' == __name__:
+    main()
