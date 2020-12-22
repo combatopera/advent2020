13e4369aedd97cc2c62e7eb5be30a809273ac919
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-12-22 12:30:36 +0000

    refactor

--- 22a.py
+++ 22a.py
@@ -5,15 +5,16 @@ from pathlib import Path
 
 def consume(decks):
     while True:
-        cards = sorted(((d.pop(0), d) for d in decks), key = lambda t: -t[0])
+        cards = [(d.pop(0), d) for d in decks]
+        cards.sort(key = lambda t: -t[0])
         cards[0][1].extend(c for c, _ in cards)
         if any(not d for _, d in cards[1:]):
-            return sum(c * (1 + i) for i, c in enumerate(reversed(cards[0][1])))
+            return decks.index(cards[0][1])
 
 def main():
     with Path('input', '22').open() as f:
         decks = [[int(l) for l in chunk[1:]] for chunk in readchunks(f)]
-    print(consume(decks))
+    print(sum(c * (1 + i) for i, c in enumerate(reversed(decks[consume(decks)]))))
 
 if '__main__' == __name__:
     main()
