ec095f122199e49a192729c45d0442f84a312f69
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-12-22 12:19:30 +0000

    refactor

--- 22a.py
+++ 22a.py
@@ -6,8 +6,7 @@ from pathlib import Path
 def consume(decks):
     while True:
         cards = [(d.pop(0), d) for d in decks]
-        cards.sort()
-        cards.reverse()
+        cards.sort(key = lambda t: -t[0])
         cards[0][1].extend(c[0] for c in cards)
         if any(not d for _, d in cards[1:]):
             return sum(c * (1 + i) for i, c in enumerate(reversed(cards[0][1])))
