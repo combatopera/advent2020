eb6ede9f505c4c206cdddb5b5b67de78f65b7312
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-12-22 12:12:51 +0000

    refactor

--- 22a.py
+++ 22a.py
@@ -3,16 +3,19 @@
 from adventlib import readchunks
 from pathlib import Path
 
-def main():
-    with Path('input', '22').open() as f:
-        decks = [[int(l) for l in chunk[1:]] for chunk in readchunks(f)]
-    while all(decks):
+def consume(decks):
+    while True:
         cards = [(d.pop(0), d) for d in decks]
         cards.sort()
         cards.reverse()
         cards[0][1].extend(c[0] for c in cards)
-    deck, = (d for d in decks if d)
-    print(sum(c * (1 + i) for i, c in enumerate(reversed(deck))))
+        if any(not d for _, d in cards[1:]):
+            return sum(c * (1 + i) for i, c in enumerate(reversed(cards[0][1])))
+
+def main():
+    with Path('input', '22').open() as f:
+        decks = [[int(l) for l in chunk[1:]] for chunk in readchunks(f)]
+    print(consume(decks))
 
 if '__main__' == __name__:
     main()
