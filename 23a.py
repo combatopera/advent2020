767c8b30f3239fcab71a79ac535b06ce3c6768f0
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-12-23 10:13:36 +0000

    amazingly that still works

--- 23a.py
+++ 23a.py
@@ -1,38 +1,58 @@
 #!/usr/bin/env python3
 
-from itertools import islice
+from itertools import chain, islice
 
 input = '538914762'
 
+class Cup:
+
+    def __init__(self, label):
+        self.label = label
+
 class Cups:
 
-    def __init__(self, n):
-        self.labels = [1 + i for i in range(n)]
+    def __init__(self, labels, n):
+        self.labeltocup = {l: Cup(l) for l in labels}
+        for l in range(len(self.labeltocup) + 1, n + 1):
+            self.labeltocup[l] = Cup(l)
+        self.current = next(iter(self.labeltocup.values()))
+        for c, d in zip(self.labeltocup.values(), chain(islice(self.labeltocup.values(), 1, None), [self.current])):
+            c.succ = d
+            d.prev = c
         self.n = n
 
-    def relabel(self, labels):
-        for i, l in enumerate(labels):
-            self.labels[i] = l
+    def _pop3(self):
+        trio = self.current.succ
+        self.current.succ = trio.succ.succ.succ
+        self.current.succ.prev = self.current
+        return trio
 
     def move(self):
-        trio = {l: None for l in islice(self.labels, 1, 4)}
-        del self.labels[1:4]
-        label = self.labels[0]
+        trio = self._pop3()
+        badlabels = {trio.label, trio.succ.label, trio.succ.succ.label}
+        label = self.current.label
         while True:
             label = (label - 2) % self.n + 1
-            if label not in trio:
+            if label not in badlabels:
                 break
-        i = self.labels.index(label) + 1
-        self.labels[i:i] = trio
-        self.labels.append(self.labels.pop(0))
+        before = self.labeltocup[label]
+        before.succ.prev = trio.succ.succ
+        trio.succ.succ.succ = before.succ
+        before.succ = trio
+        trio.prev = before
+        self.current = self.current.succ
 
     def report(self):
-        i = self.labels.index(1) + 1
-        return ''.join(str(self.labels[k % self.n]) for k in range(i, i + self.n - 1))
+        root = self.labeltocup[1]
+        def labels():
+            d = root.succ
+            while d != root:
+                yield d.label
+                d = d.succ
+        return ''.join(map(str, labels()))
 
 def main():
-    cups = Cups(len(input))
-    cups.relabel(map(int, input))
+    cups = Cups(map(int, input), len(input))
     for _ in range(100):
         cups.move()
     print(cups.report())
