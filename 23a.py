4aeb702da8cfcc14d97af86dec8e3694651cb993
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-12-23 09:34:07 +0000

    ren

--- 23a.py
+++ 23a.py
@@ -7,30 +7,30 @@ input = '538914762'
 class Cups:
 
     def __init__(self, n):
-        self.cups = [1 + i for i in range(n)]
+        self.labels = [1 + i for i in range(n)]
         self.n = n
 
     def load(self, labels):
         for i, l in enumerate(labels):
-            self.cups[i] = l
+            self.labels[i] = l
 
     def __getitem__(self, i):
-        return self.cups[i % self.n]
+        return self.labels[i % self.n]
 
     def move(self):
-        trio = {l: None for l in islice(self.cups, 1, 4)}
-        del self.cups[1:4]
-        label = self.cups[0]
+        trio = {l: None for l in islice(self.labels, 1, 4)}
+        del self.labels[1:4]
+        label = self.labels[0]
         while True:
             label = (label - 2) % self.n + 1
             if label not in trio:
                 break
-        i = self.cups.index(label) + 1
-        self.cups[i:i] = trio
-        self.cups.append(self.cups.pop(0))
+        i = self.labels.index(label) + 1
+        self.labels[i:i] = trio
+        self.labels.append(self.labels.pop(0))
 
     def report(self):
-        i = self.cups.index(1) + 1
+        i = self.labels.index(1) + 1
         return ''.join(str(self[i + k]) for k in range(self.n - 1))
 
 def main():
