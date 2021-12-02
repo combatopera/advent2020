ab3cf1d4a1698c00e4b73e7797b86220e6474ac8
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-12-02 10:05:43 +0000

    ok

---
+++ 2021/2b.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from itertools import islice
+from pathlib import Path
+
+class Pos:
+
+    h, d, a = 0, 0, 0
+
+    def forward(self, k):
+        self.h += k
+        self.d += self.a * k
+
+    def down(self, k):
+        self.a += k
+
+    def up(self, k):
+        self.a -= k
+
+def main():
+    p = Pos()
+    with Path('input', '2').open() as f:
+        for line in f:
+            w, k = line.split()
+            getattr(p, w)(int(k))
+    print(p.h * p.d)
+
+if '__main__' == __name__:
+    main()
