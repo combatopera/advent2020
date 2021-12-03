e8af19be4560d7cbdbed53af51d298c2e5ac72b9
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-12-03 10:28:11 +0000

    works

---
+++ 2021/3b.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+
+def main():
+    def rating(common):
+        lines = alllines
+        for i in range(len(lines[0])):
+            threshold = sum(ord(c) for c in '01') * len(lines) / 2
+            s = sum(ord(l[i]) for l in lines)
+            if s > threshold:
+                keep = int(common)
+            elif s < threshold:
+                keep = 1 - common
+            else:
+                keep = int(common)
+            keep = str(keep)
+            lines = [l for l in lines if l[i] == keep]
+            print(keep, len(lines))
+            if 1 == len(lines):
+                return int(lines[0], 2)
+    alllines = Path('input', '3').read_text().splitlines()
+    print(rating(True) * rating(False))
+
+if '__main__' == __name__:
+    main()
