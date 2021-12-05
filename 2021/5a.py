db11f007c635c431a67cefae2d1c617faeb0d015
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-12-05 04:56:19 +0000

    skel

---
+++ 2021/5a.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from adventlib import readchunks
+from pathlib import Path
+
+def main():
+    with Path('input', '5').open() as f:
+        pass
+
+if '__main__' == __name__:
+    main()
