adbfdfcf62d3265a65a1a3d4ece153908c5cea5e
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-12-05 05:13:26 +0000

    UGLY

--- 2021/5a.py
+++ 2021/5a.py
@@ -1,11 +1,38 @@
 #!/usr/bin/env python3
 
-from adventlib import readchunks
+from collections import defaultdict
 from pathlib import Path
 
+class Diagram:
+
+    def __init__(self):
+        self.points = defaultdict(int)
+
+    def line(self, start, end):
+        dx = end[0] - start[0]
+        dy = end[1] - start[1]
+        if dx and dy:
+            return
+        l = (dx**2+dy**2)**.5
+        v = dx / l, dy / l
+        x, y = start
+        while True:
+            self.points[x, y] += 1
+            if [x, y] == end:
+                break
+            x += v[0]
+            y += v[1]
+
+def _parsepoint(s):
+    return [int(x) for x in s.split(',')]
+
 def main():
+    d = Diagram()
     with Path('input', '5').open() as f:
-        pass
+        for line in f:
+            start, _, end = line.split()
+            d.line(*map(_parsepoint, [start, end]))
+    print(sum(1 for p, n in d.points.items() if n >= 2))
 
 if '__main__' == __name__:
     main()
