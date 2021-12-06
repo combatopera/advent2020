0894239f2bf0467dfcb76c877a3434ef1f5935a6
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-12-06 09:24:07 +0000

    oops

--- 2021/6a.py
+++ 2021/6a.py
@@ -14,7 +14,7 @@ def main():
                 fish_[timer - 1] += n
             else:
                 fish_[6] += n
-                fish_[8] += 1
+                fish_[8] += n
         fish = fish_
     print(sum(fish.values()))
 
