fe79f9f38a786d0dc79e40f56ad9947a4e6e92df
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-12-07 05:05:21 +0000

    works

---
+++ 2021/7a.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from collections import defaultdict
+from pathlib import Path
+
+def main():
+    positions = list(map(int, Path('input', '7').read_text().split(',')))
+    def fuels():
+        for target in range(min(positions), max(positions)+1):
+            yield sum(abs(p-target) for p in positions)
+    print(min(fuels()))
+
+if '__main__' == __name__:
+    main()
