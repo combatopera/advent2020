f6bc709f7c798c3742c207edc075afc4973e506e
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-12-08 12:41:22 +0000

    refactoring

--- 2021/8b.py
+++ 2021/8b.py
@@ -9,26 +9,12 @@ class Figure:
         self.segments = {i for i, c in enumerate(segments) if c != ' '}
         self.digit = digit
 
-figures = [
-    Figure(0, '-|| ||-'),
-    Figure(1, '  |  | '),
-    Figure(2, '- |-| -'),
-    Figure(3, '- |- |-'),
-    Figure(4, ' ||- | '),
-    Figure(5, '-| - |-'),
-    Figure(6, '-| -||-'),
-    Figure(7, '- |  | '),
-    Figure(8, '-||-||-'),
-    Figure(9, '-||- |-'),
-]
-figurelookup = {frozenset(f.segments): f for f in figures}
-
 class Patch:
 
     def __init__(self, chartosegment):
         self.chartosegment = chartosegment
 
-    def patches(self, figure, pattern):
+    def _patches(self, figure, pattern):
         if len(figure.segments) != len(pattern):
             return
         knownsegments = {i for c in pattern for i in [self.chartosegment.get(c)] if i is not None}
@@ -45,20 +31,35 @@ class Patch:
     def decode(self, patterns):
         return sum(10 ** i * self._decodeone(p) for i, p in enumerate(reversed(patterns)))
 
-def _patchrec(patterns, figures, patch):
-    if patterns:
-        for f in figures:
-            for q in patch.patches(f, patterns[0]):
-                yield from _patchrec(patterns[1:], figures - {f}, q)
-    else:
-        yield patch
+    def search(self, patterns, figures):
+        if patterns:
+            for f in figures:
+                for q in self._patches(f, patterns[0]):
+                    yield from q.search(patterns[1:], figures - {f})
+        else:
+            yield self
+
+figures = [
+    Figure(0, '-|| ||-'),
+    Figure(1, '  |  | '),
+    Figure(2, '- |-| -'),
+    Figure(3, '- |- |-'),
+    Figure(4, ' ||- | '),
+    Figure(5, '-| - |-'),
+    Figure(6, '-| -||-'),
+    Figure(7, '- |  | '),
+    Figure(8, '-||-||-'),
+    Figure(9, '-||- |-'),
+]
+figurelookup = {frozenset(f.segments): f for f in figures}
+emptypatch = Patch({})
 
 def main():
     n = 0
     with Path('input', '8').open() as f:
         for line in f:
             patterns, digits = (s.split() for s in line.split('|'))
-            patch, = _patchrec(sorted(patterns, key = lambda p: len(p)), set(figures), Patch({}))
+            patch, = emptypatch.search(sorted(patterns, key = lambda p: len(p)), set(figures))
             n += patch.decode(digits)
     print(n)
 
