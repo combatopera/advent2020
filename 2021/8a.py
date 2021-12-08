ec574b7b8f47d16f08da9edacb3ac7ab92ab9ada
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-12-08 05:08:05 +0000

    ok

--- 2021/8a.py
+++ 2021/8a.py
@@ -3,10 +3,13 @@
 from pathlib import Path
 
 def main():
+    n = 0
     with Path('input', '8').open() as f:
         for line in f:
             patterns, digits = (s.split() for s in line.split('|'))
-            print(patterns, digits)
+            for d in digits:
+                n += len(d) in {2, 4, 3, 7}
+    print(n)
 
 if '__main__' == __name__:
     main()
