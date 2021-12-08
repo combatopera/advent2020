200a2f1e440f64523c5c1bc0abde717de343f6af
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-12-08 11:49:19 +0000

    unused

--- 2021/8a.py
+++ 2021/8a.py
@@ -6,7 +6,7 @@ def main():
     n = 0
     with Path('input', '8').open() as f:
         for line in f:
-            patterns, digits = (s.split() for s in line.split('|'))
+            _, digits = (s.split() for s in line.split('|'))
             for d in digits:
                 n += len(d) in {2, 4, 3, 7}
     print(n)
