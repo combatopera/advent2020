364886a29fb85b8892f42fbc79f9010913dd5613
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-12-10 10:42:05 +0000

    not that exciting

---
+++ 2021/10b.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+from adventlib import intcos, intsin, Vector
+from pathlib import Path
+
+class Pair:
+
+    def __init__(self, pair, score):
+        self.open, self.close = pair
+        self.score = score
+
+pairs = {p.open: p for p in [
+    Pair('()', 1),
+    Pair('[]', 2),
+    Pair('{}', 3),
+    Pair('<>', 4),
+]}
+
+def _score(line):
+    stack = []
+    for c in line:
+        p = pairs.get(c)
+        if p is None:
+            if stack.pop(-1).close != c:
+                return
+        else:
+            stack.append(p)
+    score = 0
+    for p in reversed(stack):
+        score = score * 5 + p.score
+    return score
+
+def main():
+    scores = []
+    with Path('input', '10').open() as f:
+        for line in f:
+            s = _score(line.rstrip())
+            if s is not None:
+                scores.append(s)
+    scores.sort()
+    print(scores[len(scores) // 2])
+
+if '__main__' == __name__:
+    main()
