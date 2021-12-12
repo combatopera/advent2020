679714f95df1f17a9df43dc63dbccfc49cd668cf
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-12-12 10:59:42 +0000

    better

--- 2021/12b.py
+++ 2021/12b.py
@@ -8,23 +8,20 @@ class Graph:
         self.smallnodes = {n for e in edges for n in e if n == n.lower()}
         self.edges = edges
 
-    def _paths(self, p, restrict):
+    def _paths(self, p, visited, visited2):
         if p[-1] == 'end':
             yield p
         else:
             for e in self.edges:
                 if p[-1] in e:
                     n, = (n for n in e if n != p[-1])
-                    if 'start' != n:
-                        if n in restrict:
-                            k = restrict[n]
-                            if not k or 2 not in restrict.values():
-                                yield from self._paths(p + [n], dict(restrict, **{n: k + 1}))
-                        else:
-                            yield from self._paths(p + [n], restrict)
+                    if not (n in self.smallnodes and n in visited):
+                        yield from self._paths(p + [n], visited | {n}, visited2)
+                    elif 'start' != n and not visited2:
+                        yield from self._paths(p + [n], visited, {n})
 
     def paths(self):
-        yield from self._paths(['start'], {n: 0 for n in self.smallnodes})
+        yield from self._paths(['start'], {'start'}, set())
 
 def main():
     g = Graph([set(line.split('-')) for line in Path('input', '12').read_text().splitlines()])
