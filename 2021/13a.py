60a5239a30df058b4912855552ba8fd4c163363c
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-12-13 12:43:19 +0000

    not needed

--- 2021/13a.py
+++ 2021/13a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from adventlib import readchunks, Vector
+from adventlib import readchunks
 from pathlib import Path
 
 class Paper(set):
@@ -14,7 +14,7 @@ class Paper(set):
 def main():
     with Path('input', '13').open() as f:
         dots, folds = readchunks(f)
-    paper = Paper({Vector(map(int, d.split(','))) for d in dots})
+    paper = Paper({tuple(map(int, d.split(','))) for d in dots})
     for fold in folds[:1]:
         axis, n = fold.split('=')
         paper.fold(0 if 'x' == axis[-1] else 1, int(n))
