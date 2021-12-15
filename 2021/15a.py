a88a2f78f7132f1c8aec19c1ad0c5c497a1eb801
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-12-15 06:42:55 +0000

    faster

--- 2021/15a.py
+++ 2021/15a.py
@@ -14,6 +14,9 @@ def main():
     target = Vector([x, y])
     p = Vector([0, 0])
     labels = {q: 0 if q == p else float('inf') for q in grid}
+    costs = defaultdict(set)
+    costs[0] = {p}
+    costs[float('inf')] = grid.keys() - {p}
     while p != target:
         for s in steps:
             q = p + s
@@ -23,12 +26,18 @@ def main():
                 pass
             else:
                 if q in labels:
-                    labels[q] = min(labels[q], labels[p] + r)
+                    old = labels[q]
+                    new = min(old, labels[p] + r)
+                    labels[q] = new
+                    costs[old].remove(q)
+                    costs[new].add(q)
+        l=labels[p]
+        costs[l].remove(p)
+        if not costs[l]:
+            costs.pop(l)
         labels.pop(p)
-        mincost = min(labels.values())
-        for p in labels:
-            if labels[p] == mincost:
-                break
+        mincost = min(costs)
+        p = next(iter(costs[mincost]))
     print(labels[p])
 
 if '__main__' == __name__:
