155d441fe12004f609872515056447ce2a35cdd8
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-12-15 22:41:46 +0000

    oo

--- 2021/15a.py
+++ 2021/15a.py
@@ -6,6 +6,16 @@ from pathlib import Path
 
 steps = [Vector([intcos(x), intsin(x)]) for x in range(4)]
 
+class Labels:
+
+    def __init__(self, grid, start):
+        self.labels = {}
+        self.costs = defaultdict(set)
+        for q in grid:
+            cost = 0 if q == start else float('inf')
+            self.labels[q] = cost
+            self.costs[cost].add(q)
+
 def main():
     grid = {}
     for y, line in enumerate(Path('input', '15').read_text().splitlines()):
@@ -13,10 +23,7 @@ def main():
             grid[Vector([x, y])] = int(c)
     target = Vector([x, y])
     p = Vector([0, 0])
-    labels = {q: 0 if q == p else float('inf') for q in grid}
-    costs = defaultdict(set)
-    for q, cost in labels.items():
-        costs[cost].add(q)
+    labels = Labels(grid, p)
     while p != target:
         for s in steps:
             q = p + s
@@ -25,20 +32,20 @@ def main():
             except KeyError:
                 pass
             else:
-                if q in labels:
-                    old = labels[q]
-                    new = min(old, labels[p] + r)
-                    labels[q] = new
-                    costs[old].remove(q)
-                    costs[new].add(q)
-        l=labels[p]
-        costs[l].remove(p)
-        if not costs[l]:
-            costs.pop(l)
-        labels.pop(p)
-        mincost = min(costs)
-        p = next(iter(costs[mincost]))
-    print(labels[p])
+                if q in labels.labels:
+                    old = labels.labels[q]
+                    new = min(old, labels.labels[p] + r)
+                    labels.labels[q] = new
+                    labels.costs[old].remove(q)
+                    labels.costs[new].add(q)
+        l = labels.labels[p]
+        labels.costs[l].remove(p)
+        if not labels.costs[l]:
+            labels.costs.pop(l)
+        labels.labels.pop(p)
+        mincost = min(labels.costs)
+        p = next(iter(labels.costs[mincost]))
+    print(labels.labels[p])
 
 if '__main__' == __name__:
     main()
