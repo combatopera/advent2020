942b66ba17ad3944f42b4367550579bc9a902d59
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-12-15 22:44:28 +0000

    refactoring

--- 2021/15a.py
+++ 2021/15a.py
@@ -6,46 +6,46 @@ from pathlib import Path
 
 steps = [Vector([intcos(x), intsin(x)]) for x in range(4)]
 
-class Labels:
+class State:
 
-    def __init__(self, grid, start):
+    def __init__(self, points, start):
         self.labels = {}
         self.costs = defaultdict(set)
-        for q in grid:
-            cost = 0 if q == start else float('inf')
-            self.labels[q] = cost
-            self.costs[cost].add(q)
+        for p in points:
+            cost = 0 if p == start else float('inf')
+            self.labels[p] = cost
+            self.costs[cost].add(p)
 
 def main():
-    grid = {}
+    weights = {}
     for y, line in enumerate(Path('input', '15').read_text().splitlines()):
         for x, c in enumerate(line):
-            grid[Vector([x, y])] = int(c)
+            weights[Vector([x, y])] = int(c)
     target = Vector([x, y])
     p = Vector([0, 0])
-    labels = Labels(grid, p)
+    state = State(weights, p)
     while p != target:
         for s in steps:
             q = p + s
             try:
-                r = grid[q]
+                r = weights[q]
             except KeyError:
                 pass
             else:
-                if q in labels.labels:
-                    old = labels.labels[q]
-                    new = min(old, labels.labels[p] + r)
-                    labels.labels[q] = new
-                    labels.costs[old].remove(q)
-                    labels.costs[new].add(q)
-        l = labels.labels[p]
-        labels.costs[l].remove(p)
-        if not labels.costs[l]:
-            labels.costs.pop(l)
-        labels.labels.pop(p)
-        mincost = min(labels.costs)
-        p = next(iter(labels.costs[mincost]))
-    print(labels.labels[p])
+                if q in state.labels:
+                    old = state.labels[q]
+                    new = min(old, state.labels[p] + r)
+                    state.labels[q] = new
+                    state.costs[old].remove(q)
+                    state.costs[new].add(q)
+        l = state.labels[p]
+        state.costs[l].remove(p)
+        if not state.costs[l]:
+            state.costs.pop(l)
+        state.labels.pop(p)
+        mincost = min(state.costs)
+        p = next(iter(state.costs[mincost]))
+    print(state.labels[p])
 
 if '__main__' == __name__:
     main()
