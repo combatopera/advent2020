1503da6ea6420020db42019d5bd1b70467cb9bbe
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-12-17 13:29:46 +0000

    need to automate these bounds somehow

---
+++ 2021/17b.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+import re
+
+class Probe:
+
+    x = y = 0
+
+    def __init__(self, u, v):
+        self.u = u
+        self.v = v
+
+    def step(self):
+        self.x += self.u
+        self.y += self.v
+        self.u = max(0, self.u - 1)
+        self.v -= 1
+
+class Target:
+
+    def __init__(self, x1, x2, y1, y2):
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+
+    def _height(self, u, v):
+        p = Probe(u, v)
+        maxheight = p.y
+        while True:
+            p.step()
+            if p.y > maxheight:
+                maxheight = p.y
+                continue
+            if p.x > self.x2 or p.y < self.y1:
+                break
+            if p.x >= self.x1 and p.y <= self.y2:
+                return maxheight
+
+    def heights(self, u):
+        v = -200
+        while v < 300:
+            h = self._height(u, v)
+            if h is not None:
+                yield h
+            v += 1
+
+def main():
+    t = Target(*map(int, re.findall('-?[0-9]+', Path('input', '17').read_text())))
+    heights = []
+    for u in range(400):
+        heights.extend(t.heights(u))
+        u += 1
+    print(len(heights))
+
+if '__main__' == __name__:
+    main()
