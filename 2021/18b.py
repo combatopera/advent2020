05765c261f5997ce6587d78da08a1304ad775fe9
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-12-18 22:41:51 +0000

    slower but nicer

--- 2021/18b.py
+++ 2021/18b.py
@@ -1,81 +1,85 @@
 #!/usr/bin/env python3
 
 from collections import namedtuple
-from copy import deepcopy
 from pathlib import Path
 
-class Address(namedtuple('BaseAddress', 'number index')): pass
+class Address(namedtuple('BaseAddress', 'number index')):
+
+    def mirror(self):
+        return type(self)(self.number, 1 - self.index)
+
+    def replace(self, n):
+        self.number[self.index] = n
+
+    def __getattr__(self, name):
+        return lambda *args: getattr(self.number[self.index], name)(self, *args)
+
+class Int(int):
+
+    def clone(self):
+        return self
+
+    def explode(self, *context):
+        pass
+
+    def addimpl(self, address, index, n):
+        address.replace(type(self)(self + n))
+
+    def split(self, address):
+        if self >= 10:
+            address.replace(Number([type(self)(self // 2), type(self)((self + 1) // 2)]))
+            return True
+
+    def magnitude(self):
+        return self
+
+zero = Int(0)
 
 class Number(list):
-
-    class Int(int):
-
-        def explode(self, *context):
-            pass
-
-        def addimpl(self, address, n, target):
-            address.number[address.index] = type(self)(self + n)
-
-        def split(self, address):
-            if self >= 10:
-                address.number[address.index] = Number([type(self)(self // 2), type(self)((self + 1) // 2)])
-                return True
-
-        def magnitude(self):
-            return self
-
-    zero = Int(0)
 
     @classmethod
     def xform(cls, obj):
         try:
             return cls(map(cls.xform, obj))
         except TypeError:
-            return cls.Int(obj)
+            return Int(obj)
 
-    def add(self, n):
-        self[:] = (type(self)(self), n) if self else n
-        while self.explode() or self.split(None):
+    def _addresses(self):
+        for i in range(len(self)):
+            yield Address(self, i)
+
+    def clone(self):
+        return type(self)(n.clone() for n in self)
+
+    def add(self, that):
+        res = type(self)(n.clone() for n in [self, that])
+        while res.explode() or res.split(None):
             pass
+        return res
 
     def explode(self, *context):
         if 4 != len(context):
-            return any(n.explode(Address(self, i), *context) for i, n in enumerate(self))
+            return any(a.explode(*context) for a in self._addresses())
         for index, n in enumerate(self):
             for address in context:
                 if address.index == 1 - index:
-
-                    #print(address.number[index], '.add', Address(address.number, index), n)
-
-                    address.number[index].addimpl(Address(address.number, index), n, 1 - index)
+                    address.mirror().addimpl(1 - index, n)
                     break
-
-
-
-        context[0].number[context[0].index] = self.zero
+        context[0].replace(zero)
         return True
 
-    def addimpl(self, address, n, target):
-        self[target].addimpl(Address(self, target), n, target)
+    def addimpl(self, address, index, n):
+        Address(self, index).addimpl(index, n)
 
     def split(self, address):
-        return any(n.split(Address(self, i)) for i, n in enumerate(self))
+        return any(a.split() for a in self._addresses())
 
     def magnitude(self):
-        return 3 * self[0].magnitude() + 2 * self[1].magnitude()
+        return sum(k * n.magnitude() for k, n in zip([3, 2], self))
 
 def main():
-    lines = Path('input', '18').read_text().splitlines()
-    def mags():
-        for a in lines:
-            for b in lines:
-                if a != b:
-                    m = Number.xform(eval(a))
-                    m.add(Number.xform(eval(b)))
-                    yield m.magnitude()
-
-
-    print(max(mags()))
+    numbers = [Number.xform(eval(l)) for l in Path('input', '18').read_text().splitlines()]
+    print(max(m.add(n).magnitude() for m in numbers for n in numbers if m is not n))
 
 if '__main__' == __name__:
     main()
