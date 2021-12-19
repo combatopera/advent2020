2bae6271caa938675d20948607bf30a4341b3b18
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-12-19 16:30:03 +0000

    why does it not work

--- 2021/19a.py
+++ 2021/19a.py
@@ -3,23 +3,33 @@
 from adventlib import readchunks, Vector
 from pathlib import Path
 
+def _getmap(u, v):
+    (a, i), = ([a, i] for i in range(3) for a in [-1, 1] if a * u[i] == v[0])
+    (b, j), = ([a, i] for i in range(3) for a in [-1, 1] if a * u[i] == v[1])
+    (c, k), = ([a, i] for i in range(3) for a in [-1, 1] if a * u[i] == v[2])
+    return lambda u: Vector([a * u[i], b * u[j], c * u[k]])
+
 class Scanner:
 
-    def __init__(self, points):
-        self.characters = [{tuple(sorted(map(abs, p - com))): p for p in points} for com in points]
-        assert all(len(c) == len(points) for c in self.characters)
-        self.points = points
+    def __init__(self, beacons):
+        self.characters = [{tuple(sorted(map(abs, b - o))): b for b in beacons} for o in beacons]
+        assert all(len(c) == len(beacons) for c in self.characters)
+        self.beacons = beacons
 
     def reorient(self, that):
-        def match():
-            for c in self.characters:
-                for d in that.characters:
-                    if len(c.keys() & d.keys()) >= 12:
-                        return True
-        if not match():
-            return
-        ...
-        return True
+        for c in self.characters:
+            for d in that.characters:
+                keys = c.keys() & d.keys()
+                if len(keys) >= 12:
+                    co = c[0, 0, 0]
+                    do = d[0, 0, 0]
+                    for k in keys:
+                        if any(k):
+                            m = _getmap(c[k] - co, d[k] - do)
+                            break
+                    assert {k: d[k] for k in keys} == {k: m(c[k]-co)+do for k in keys}
+                    self.beacons = [m(b - co) + do for b in self.beacons]
+                    return True
 
 def main():
     with Path('input', '19').open() as f:
@@ -36,6 +46,12 @@ def main():
                     oriented_.add(s)
                     break
         oriented |= oriented_
+    from collections import Counter
+    c = Counter()
+    for s in scanners:
+        for b in s.beacons:
+            c[b] += 1
+    print(len(c))
 
 if '__main__' == __name__:
     main()
