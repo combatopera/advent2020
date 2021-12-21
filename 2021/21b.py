b937d917d31f5041ff915e75436625735e918a6b
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-12-21 10:47:39 +0000

    works

--- 2021/21b.py
+++ 2021/21b.py
@@ -3,37 +3,49 @@
 from collections import Counter, namedtuple
 from pathlib import Path
 
+winscore = 21
 die = Counter()
 for a in range(1, 4):
     for b in range(1, 4):
         for c in range(1, 4):
             die[a+b+c] += 1
 
-class State(namedtuple('BaseState', 'pos score')):
+class Game(namedtuple('BaseGame', 'p1 s1 p2 s2')):
 
-    def next(self):
-        for total, n in die.items():
-            pos = (self.pos + total - 1) % 10 + 1
-            yield type(self)(pos, self.score + pos), n
+    def play1(self):
+        for move, n in die.items():
+            p1 = (self.p1 + move - 1) % 10 + 1
+            yield type(self)(p1, self.s1 + p1, self.p2, self.s2), n
 
-class Player(dict):
-
-    wins = 0
+    def play2(self):
+        for move, n in die.items():
+            p2 = (self.p2 + move - 1) % 10 + 1
+            yield type(self)(self.p1, self.s1, p2, self.s2 + p2), n
 
 def main():
-    players = [Player({State(int(l.split(':')[-1]), 0): 1}) for l in Path('input', '21').read_text().splitlines()]
-    while any(state.score < 21 for p in players for state in p):
-        for p in players:
-            items = list(p.items())
-            p.clear()
-            for state, m in items:
-                for state_, n in state.next():
-                    n *= m
-                    if state_.score < 21:
-                        p[state_] = n
-                    else:
-                        p.wins += n
-    print(max(p.wins for p in players))
+    p1, p2 = (int(l.split(':')[-1]) for l in Path('input', '21').read_text().splitlines())
+    games = Counter({Game(p1, 0, p2, 0): 1})
+    wins1 = wins2 = 0
+    while games:
+        items = list(games.items())
+        games.clear()
+        for og, m in items:
+            for g, n in og.play1():
+                n *= m
+                if g.s1 >= winscore:
+                    wins1 += n
+                else:
+                    games[g] += n
+        items = list(games.items())
+        games.clear()
+        for og, m in items:
+            for g, n in og.play2():
+                n *= m
+                if g.s2 >= winscore:
+                    wins2 += n
+                else:
+                    games[g] += n
+    print(max(wins1, wins2))
 
 if '__main__' == __name__:
     main()
