eb676907098aea43e8362a99764bf05fdb9e5992
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-12-21 10:54:47 +0000

    undup

--- 2021/21b.py
+++ 2021/21b.py
@@ -12,40 +12,32 @@ for a in range(1, 4):
 
 class Game(namedtuple('BaseGame', 'p1 s1 p2 s2')):
 
-    def play1(self):
+    def play(self, player):
         for move, n in die.items():
-            p1 = (self.p1 + move - 1) % 10 + 1
-            yield type(self)(p1, self.s1 + p1, self.p2, self.s2), n
+            g = list(self)
+            g[player * 2] = pos = (self[player * 2] + move - 1) % 10 + 1
+            g[player * 2 + 1] += pos
+            yield type(self)(*g), n
 
-    def play2(self):
-        for move, n in die.items():
-            p2 = (self.p2 + move - 1) % 10 + 1
-            yield type(self)(self.p1, self.s1, p2, self.s2 + p2), n
+    def score(self, player):
+        return self[player * 2 + 1]
 
 def main():
     p1, p2 = (int(l.split(':')[-1]) for l in Path('input', '21').read_text().splitlines())
     games = Counter({Game(p1, 0, p2, 0): 1})
-    wins1 = wins2 = 0
+    wins = [0, 0]
     while games:
-        items = list(games.items())
-        games.clear()
-        for og, m in items:
-            for g, n in og.play1():
-                n *= m
-                if g.s1 >= winscore:
-                    wins1 += n
-                else:
-                    games[g] += n
-        items = list(games.items())
-        games.clear()
-        for og, m in items:
-            for g, n in og.play2():
-                n *= m
-                if g.s2 >= winscore:
-                    wins2 += n
-                else:
-                    games[g] += n
-    print(max(wins1, wins2))
+        for player in range(2):
+            items = list(games.items())
+            games.clear()
+            for og, m in items:
+                for g, n in og.play(player):
+                    n *= m
+                    if g.score(player) >= winscore:
+                        wins[player] += n
+                    else:
+                        games[g] += n
+    print(max(wins))
 
 if '__main__' == __name__:
     main()
