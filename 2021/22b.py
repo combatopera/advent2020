91cda00cf0dbaad0ac3d0723af36618886e7ed30
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-12-22 19:27:35 +0000

    refactoring

--- 2021/22b.py
+++ 2021/22b.py
@@ -17,18 +17,18 @@ class Box:
         return self.x1 < self.x2 and self.y1 < self.y2 and self.z1 < self.z2
 
     def sub(self, that):
-        if self.x2 <= that.x1 or that.x2 <= self.x1 or self.y2 <= that.y1 or that.y2 <= self.y1 or self.z2 <= that.z1 or that.z2 <= self.z1:
+        if self.x2 > that.x1 and that.x2 > self.x1 and self.y2 > that.y1 and that.y2 > self.y1 and self.z2 > that.z1 and that.z2 > self.z1:
+            w1 = type(self)(self.x1, self.x2, self.y1, self.y2, self.z1, that.z1)
+            w2 = type(self)(self.x1, self.x2, self.y1, self.y2, that.z2, self.z2)
+            u1 = type(self)(self.x1, that.x1, self.y1, self.y2, max(self.z1, that.z1), min(self.z2, that.z2))
+            u2 = type(self)(that.x2, self.x2, self.y1, self.y2, max(self.z1, that.z1), min(self.z2, that.z2))
+            v1 = type(self)(max(self.x1, that.x1), min(self.x2, that.x2), self.y1, that.y1, max(self.z1, that.z1), min(self.z2, that.z2))
+            v2 = type(self)(max(self.x1, that.x1), min(self.x2, that.x2), that.y2, self.y2, max(self.z1, that.z1), min(self.z2, that.z2))
+            for b in w1, w2, u1, u2, v1, v2:
+                if b.valid():
+                    yield b
+        else:
             yield self
-            return
-        w1 = type(self)(self.x1, self.x2, self.y1, self.y2, self.z1, that.z1)
-        w2 = type(self)(self.x1, self.x2, self.y1, self.y2, that.z2, self.z2)
-        u1 = type(self)(self.x1, that.x1, self.y1, self.y2, max(self.z1, that.z1), min(self.z2, that.z2))
-        u2 = type(self)(that.x2, self.x2, self.y1, self.y2, max(self.z1, that.z1), min(self.z2, that.z2))
-        v1 = type(self)(max(self.x1, that.x1), min(self.x2, that.x2), self.y1, that.y1, max(self.z1, that.z1), min(self.z2, that.z2))
-        v2 = type(self)(max(self.x1, that.x1), min(self.x2, that.x2), that.y2, self.y2, max(self.z1, that.z1), min(self.z2, that.z2))
-        for b in w1, w2, u1, u2, v1, v2:
-            if b.valid():
-                yield b
 
     def volume(self):
         return (self.x2 - self.x1) * (self.y2 - self.y1) * (self.z2 - self.z1)
