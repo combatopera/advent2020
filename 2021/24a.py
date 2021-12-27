d380f044b7c3ef10725d00a687e7a16532646bc1
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-12-27 12:47:48 +0000

    wip

--- 2021/24a.py
+++ 2021/24a.py
@@ -2,60 +2,60 @@
 
 from pathlib import Path
 
-def comp(name, instructions):
-    assert 'inp w' == instructions[0]
-    for c in 'xy':
-        for s in instructions:
-            if c in s:
-                assert f"mul {c} 0" == s
-                break
-    def g():
-        yield 'x = y = 0'
-        for s in instructions[1:]:
-            s = s.split()
-            assert 'inp' != s[0]
-            if 'add' == s[0]:
-                yield f"{s[1]} += {s[2]}"
-            elif 'mul' == s[0]:
-                yield f"{s[1]} *= {s[2]}"
-            elif 'div' == s[0]:
-                yield f"{s[1]} = int({s[1]} / {s[2]})"
-            elif 'mod' == s[0]:
-                yield f"{s[1]} %= {s[2]}"
-            elif 'eql' == s[0]:
-                yield f"{s[1]} = int({s[1]} == {s[2]})"
-            else:
-                raise Exception
-    text = ''.join(f"{l}\n" for l in g())
-    print(text)
-    return compile(text, name, 'exec')
+class NeverMind(Exception): pass
+
+class Digit:
+
+    def __init__(self, index, instructions):
+        assert 'inp w' == instructions[0]
+        for c in 'xy':
+            for s in instructions:
+                if c in s:
+                    assert f"mul {c} 0" == s
+                    break
+        def g():
+            yield 'x = y = 0'
+            for s in instructions[1:]:
+                s = s.split()
+                assert 'inp' != s[0]
+                if 'add' == s[0]:
+                    yield f"{s[1]} += {s[2]}"
+                elif 'mul' == s[0]:
+                    yield f"{s[1]} *= {s[2]}"
+                elif 'div' == s[0]:
+                    yield f"{s[1]} = {s[1]} // {s[2]}"
+                elif 'mod' == s[0]:
+                    yield f"if {s[1]} < 0: raise NeverMind"
+                    yield f"{s[1]} %= {s[2]}"
+                elif 'eql' == s[0]:
+                    yield f"{s[1]} = {s[1]} == {s[2]}"
+                else:
+                    raise Exception
+        self.code = compile('\n'.join(g()), str(index), 'exec')
+        self.index = index
+
+    def getz(self, z, w):
+        g = dict(NeverMind = NeverMind, z = z, w = w)
+        try:
+            exec(self.code, g)
+            return g['z']
+        except NeverMind:
+            pass
 
 def main():
     lines = Path('input', '24').read_text().splitlines()
     inps = [i for i, l in enumerate(lines) if l.split()[0] == 'inp']
-    digits = [comp(str(k), lines[i:j]) for i, j, k in zip(inps, inps[1:]+[None], range(len(inps)))]
-    accepts = {(len(inps), w): {0} for w in range(1, 10)}
-    for i, d in reversed(list(enumerate(digits))):
-        print(accepts)
-        for w in range(1, 10):
-            accept = accepts[i+1, w]
-            accepts[i, w] = set()
-            z = 0
-            while True:
-                g = dict(w = w, z = z)
-                exec(d, g)
-                if g['z'] in accept:
-                    accepts[i, w].add(z)
-                    break
-                z += 1
-            z = -1
-            while True:
-                g = dict(w = w, z = z)
-                exec(d, g)
-                if g['z'] in accept:
-                    accepts[i, w].add(z)
-                    break
-                z -= 1
+    digits = [Digit(k, lines[i:j]) for k, (i, j) in enumerate(zip(inps, [*inps[1:], None]))]
+    accepts = {len(inps): {0: None}}
+    for d in reversed(digits):
+        accept = accepts[d.index + 1]
+        accepts[d.index] = {}
+        for z in range(-10000, 10000):
+            for w in range(1, 10):
+                z_ = d.getz(z, w)
+                if z_ in accept:
+                    accepts[d.index][z] = w
+        print(d.index, accepts[d.index])
 
 if '__main__' == __name__:
     main()
