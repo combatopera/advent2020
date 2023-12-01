243207004d28549d4d323bc80539135e16062394
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2023-12-01 18:39:26 +0000

    more elegant maybe

--- 2023/1b.py
+++ 2023/1b.py
@@ -1,14 +1,15 @@
 from adventlib import inpath
 import re
 
-names = {n: 1 + i for i, n in enumerate('one two three four five six seven eight nine'.split())}
-regexfirst = '([0-9]|' + '|'.join(map(re.escape, names)) + ')'
-regexlast = '.*' + regexfirst
-for i in range(10):
-    names[str(i)] = i
-
 def main():
-    def g():
-        for l in inpath().open():
-            yield names[re.search(regexfirst, l).group(1)] * 10 + names[re.search(regexlast, l).group(1)]
-    print(sum(g()))
+    def search(regex, line):
+        return names[re.search(regex, line).group(1)]
+    def lineval(line):
+        return search(lregex, line) * 10 + search(rregex, line)
+    names = {}
+    for i, n in enumerate('one two three four five six seven eight nine'.split()):
+        k = 1 + i
+        names[n] = names[str(k)] = k
+    lregex = f"({'|'.join(map(re.escape, names))})"
+    rregex = f".*{lregex}"
+    print(sum(map(lineval, inpath().open())))
