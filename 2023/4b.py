756b208fb943ddef9f3621d2b99295297fde2da0
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2023-12-04 11:02:38 +0000

    efficienter

--- 2023/4b.py
+++ 2023/4b.py
@@ -11,7 +11,7 @@ def main():
             copies[i] += 1
             _, l = l.split(': ')
             l, r = l.split(' | ')
-            win = number.findall(l)
+            win = set(number.findall(l))
             n = sum(1 for x in number.findall(r) if x in win)
             for j in range(n):
                 copies[i + 1 + j] += copies[i]
