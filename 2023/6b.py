cc8fc0c8c933839abc01e9b695a8823f1ac61bb1
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2023-12-06 18:07:08 +0000

    correct i think

---
+++ 2023/6b.py
@@ -0,0 +1,20 @@
+from adventlib import inpath
+from math import ceil, floor
+import re
+
+number = re.compile('[0-9]+')
+
+def roots(a, b, c):
+    t = (b ** 2 - 4 * a * c) ** .5
+    for s in -1, 1:
+        yield (-b + s * t) / (2 * a)
+
+def main():
+    with inpath().open() as f:
+        time, distance = (int(''.join(number.findall(l))) for l in f)
+    '''
+    distance < speed * (time - speed)
+    speed ** 2 - time * speed + distance < 0
+    '''
+    lo, hi = roots(1, -time, distance)
+    print(ceil(hi - 1) - floor(lo + 1) + 1)
