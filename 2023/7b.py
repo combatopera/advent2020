e1171884db2ccaab1db5e2fb52366e522e8d8e55
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2023-12-07 18:26:24 +0000

    conciser

--- 2023/7b.py
+++ 2023/7b.py
@@ -3,36 +3,33 @@ from collections import Counter
 
 order = {c: i for i, c in enumerate('AKQT98765432J')}
 
-class Hand:
-
-    def __init__(self, text):
-        def g():
-            options = set(text) - {'J'}
-            if not options:
-                yield 0
-                return
+def key(text):
+    def g():
+        options = set(text) - {'J'}
+        if options:
             for j in options:
                 d = Counter(text.replace('J', j))
                 n = len(d)
                 if 1 == n:
-                    r = 0
+                    yield 0
                 elif 2 == n:
-                    r = 1 if 4 in d.values() else 2
+                    yield 1 if 4 in d.values() else 2
                 elif 3 == n:
-                    r = 3 if 3 in d.values() else 4
+                    yield 3 if 3 in d.values() else 4
                 elif 4 == n:
-                    r = 5
+                    yield 5
                 else:
-                    r = 6
-                yield r
-        self.key = (min(g()), *(order[c] for c in text))
+                    yield 6
+        else:
+            yield 0
+    return (min(g()), *(order[c] for c in text))
 
 def main():
     v = []
     with inpath().open() as f:
         for l in f:
             hand, bid = l.split()
-            v.append([Hand(hand), int(bid)])
-    v.sort(key = lambda t: t[0].key)
+            v.append([key(hand), int(bid)])
+    v.sort()
     v.reverse()
     print(sum(bid * (1 + i) for i, (_, bid) in enumerate(v)))
