ace87af828eee30f7759d9499a3f866b6c937e48
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2023-12-08 11:31:13 +0000

    efficienter

--- 2023/8a.py
+++ 2023/8a.py
@@ -5,10 +5,10 @@ def main():
     with inpath().open() as f:
         (top,), rest = readchunks(f)
     chart = {k: dict(L = l, R = r) for line in rest for k, l, r in [re.findall('[A-Z]{3}', line)]}
-    steps = 0
+    times = 0
     key = 'AAA'
     while 'ZZZ' != key:
         for c in top:
             key = chart[key][c]
-        steps += len(top)
-    print(steps)
+        times += 1
+    print(times * len(top))
