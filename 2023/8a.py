c6befe95022d4bbd6df9fa853bda429b9fce82c3
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2023-12-08 11:29:50 +0000

    inline

--- 2023/8a.py
+++ 2023/8a.py
@@ -1,12 +1,10 @@
 from adventlib import inpath, readchunks
 import re
 
-caps = re.compile('[A-Z]{3}')
-
 def main():
     with inpath().open() as f:
         (top,), rest = readchunks(f)
-    chart = {k: dict(L = l, R = r) for line in rest for k, l, r in [caps.findall(line)]}
+    chart = {k: dict(L = l, R = r) for line in rest for k, l, r in [re.findall('[A-Z]{3}', line)]}
     steps = 0
     key = 'AAA'
     while 'ZZZ' != key:
