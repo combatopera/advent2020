758cad611213cdd2656dc983b4acff15e4a02761
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2023-12-12 20:01:46 +0000

    getting somewhere

--- 2023/12b.py
+++ 2023/12b.py
@@ -1,30 +1,28 @@
 from adventlib import inpath
-from itertools import combinations
-import re
+from itertools import islice
 
-class Record:
+def charat(text, i):
+    return text[i] if i >= 0 and i < len(text) else '.'
 
-    def __init__(self, text):
-        self.hashes = sum(1 for c in text if '#' == c)
-        self.holes = [i for i, c in enumerate(text) if '?' == c]
-        self.text = text
-
-    def options(self, check):
-        hashes = sum(check) - self.hashes
-        for indices in combinations(self.holes, hashes):
-            v = list(self.text)
-            for i in indices:
-                v[i] = '#'
-            if list(map(len, re.findall('#+', ''.join(v)))) == check:
-                yield
+def fit(v, i, text, j):
+    if i == len(v):
+        yield ()
+    else:
+        w = v[i]
+        maxj = len(text) - (sum(islice(v, i, None)) + len(v) - i - 1)
+        while j <= maxj and '#' != charat(text, j - 1):
+            if '#' != charat(text, j + w) and all('.' != text[k] for k in range(j, j + w)):
+                for more in fit(v, i + 1, text, j + w + 1):
+                    yield [j, *more]
+            j += 1
 
 def main():
     def g():
         with inpath().open() as f:
             for l in f:
-                u, v = l.split()
-                record = Record('?'.join(u for _ in range(5)))
-                check = list(map(int, v.split(','))) * 5
-                n = sum(1 for _ in record.options(check))
-                yield n
+                record, v = l.split()
+                record = '?'.join(record for _ in range(mul))
+                check = list(map(int, v.split(','))) * mul
+                yield sum(1 for _ in fit(check, 0, record, 0))
+    mul = 5
     print(sum(g()))
