e8e69f7dff6e34de5756d15b219e792157b6c08f
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2023-12-12 17:29:32 +0000

    works

---
+++ 2023/12a.py
@@ -0,0 +1,30 @@
+from adventlib import inpath
+from itertools import combinations
+import re
+
+class Record:
+
+    def __init__(self, text):
+        self.hashes = sum(1 for c in text if '#' == c)
+        self.holes = [i for i, c in enumerate(text) if '?' == c]
+        self.text = text
+
+    def options(self, check):
+        hashes = sum(check) - self.hashes
+        for indices in combinations(self.holes, hashes):
+            v = list(self.text)
+            for i in indices:
+                v[i] = '#'
+            if list(map(len, re.findall('#+', ''.join(v)))) == check:
+                yield
+
+def main():
+    def g():
+        with inpath().open() as f:
+            for l in f:
+                u, v = l.split()
+                record = Record(u)
+                check = list(map(int, v.split(',')))
+                n = sum(1 for _ in record.options(check))
+                yield n
+    print(sum(g()))
