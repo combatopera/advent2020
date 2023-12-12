40dc0ff25db783b2066f6dbeed71dcf6c5166c94
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2023-12-12 21:07:38 +0000

    inline

--- 2023/12b.py
+++ 2023/12b.py
@@ -6,7 +6,7 @@ def charat(text, i):
     return text[i] if i >= 0 and i < len(text) else '.'
 
 @lru_cache(None)
-def ways(v, i, text, j):
+def ways(text, j, v, i):
     if i == len(v):
         return all('#' != text[k] for k in range(j, len(text)))
     w = v[i]
@@ -14,7 +14,7 @@ def ways(v, i, text, j):
     n = 0
     while j <= maxj and '#' != charat(text, j - 1):
         if '#' != charat(text, j + w) and all('.' != text[k] for k in range(j, j + w)):
-            n += ways(v, i + 1, text, j + w + 1)
+            n += ways(text, j + w + 1, v, i + 1)
         j += 1
     return n
 
@@ -22,9 +22,7 @@ def main():
     def g():
         with inpath().open() as f:
             for l in f:
-                record, v = l.split()
-                record = '?'.join(record for _ in range(mul))
-                check = tuple(map(int, v.split(','))) * mul
-                yield ways(check, 0, record, 0)
+                u, v = l.split()
+                yield ways('?'.join(u for _ in range(mul)), 0, tuple(map(int, v.split(','))) * mul, 0)
     mul = 5
     print(sum(g()))
