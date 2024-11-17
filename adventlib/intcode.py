e0a1e5084ce138c0530549639075fb2a2f45dc46
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-11-17 19:16:46 +0000

    works

--- adventlib/intcode.py
+++ adventlib/intcode.py
@@ -6,11 +6,11 @@ class Computer:
         self.data = data
 
     def opcode1(self):
-        self.data[self.data[self.pc + 3]] = self.data[self.data[self.pc + 1]] + self.data[self.data[self.pc + 2]]
+        self.data[self.data[self.pc + 3]] = self._fetch(1) + self._fetch(2)
         return 4
 
     def opcode2(self):
-        self.data[self.data[self.pc + 3]] = self.data[self.data[self.pc + 1]] * self.data[self.data[self.pc + 2]]
+        self.data[self.data[self.pc + 3]] = self._fetch(1) * self._fetch(2)
         return 4
 
     def opcode3(self):
@@ -18,13 +18,18 @@ class Computer:
         return 2
 
     def opcode4(self):
-        print(self.data[self.data[self.pc + 1]])
+        print(self._fetch(1))
         return 2
+
+    def _fetch(self, off):
+        val = self.data[self.pc + off]
+        mode = self.modes // (10 ** (off - 1)) % 10
+        return val if mode else self.data[val]
 
     def run(self):
         while True:
             k = self.data[self.pc]
             if 99 == k:
                 break
-            modes, opcode = divmod(k, 100)
+            self.modes, opcode = divmod(k, 100)
             self.pc += getattr(self, f"opcode{opcode}")()
