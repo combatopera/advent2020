5a2abde75c71c016a96ee59c589e5c3c9927e842
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-11-17 20:54:23 +0000

    yay

---
+++ 2019/7a.py
@@ -0,0 +1,15 @@
+from adventlib import inpath
+from adventlib.intcode import Computer
+from itertools import permutations
+
+def main():
+    def signals():
+        for setting in permutations(range(5)):
+            amps = [Computer(program.copy(), [phase, None]) for phase in setting]
+            signal = 0
+            for a in amps:
+                a.inputs[1] = signal
+                signal, = a
+            yield signal
+    program = [int(s) for s in inpath().read_text().split(',')]
+    print(max(signals()))
