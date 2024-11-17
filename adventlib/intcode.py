9c2050bdcf6753aad09d274b4bef0d7b0e04fe8b
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-11-17 19:09:04 +0000

    conciser

--- adventlib/intcode.py
+++ adventlib/intcode.py
@@ -1,30 +1,30 @@
 class Computer:
 
-    cursor = 0
+    pc = 0
 
-    def __init__(self, program):
-        self.program = program
+    def __init__(self, data):
+        self.data = data
 
     def opcode1(self):
-        self.program[self.program[self.cursor + 3]] = self.program[self.program[self.cursor + 1]] + self.program[self.program[self.cursor + 2]]
+        self.data[self.data[self.pc + 3]] = self.data[self.data[self.pc + 1]] + self.data[self.data[self.pc + 2]]
         return 4
 
     def opcode2(self):
-        self.program[self.program[self.cursor + 3]] = self.program[self.program[self.cursor + 1]] * self.program[self.program[self.cursor + 2]]
+        self.data[self.data[self.pc + 3]] = self.data[self.data[self.pc + 1]] * self.data[self.data[self.pc + 2]]
         return 4
 
     def opcode3(self):
-        self.program[self.program[self.cursor + 1]] = int(input('Integer: '))
+        self.data[self.data[self.pc + 1]] = int(input('Integer: '))
         return 2
 
     def opcode4(self):
-        print(self.program[self.program[self.cursor + 1]])
+        print(self.data[self.data[self.pc + 1]])
         return 2
 
     def run(self):
         while True:
-            k = self.program[self.cursor]
-            opcode = k % 100
-            if 99 == opcode:
+            k = self.data[self.pc]
+            if 99 == k:
                 break
-            self.cursor += getattr(self, f"opcode{opcode}")()
+            modes, opcode = divmod(k, 100)
+            self.pc += getattr(self, f"opcode{opcode}")()
