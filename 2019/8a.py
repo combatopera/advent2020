819a9db1017799faf9c50b4a28d8dc7095d8e43c
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-11-17 22:26:39 +0000

    gen

--- 2019/8a.py
+++ 2019/8a.py
@@ -5,12 +5,11 @@ import re
 w, h = 25, 6
 
 def main():
-    layersize = w * h
-    layers = []
-    for layer in re.findall(f".{{{layersize}}}", inpath().read_text()):
-        d = defaultdict(int)
-        for c in layer:
-            d[int(c)] += 1
-        layers.append(d)
-    l = min(layers, key = lambda l: l[0])
+    def layers():
+        for layer in re.findall(f".{{{w * h}}}", inpath().read_text()):
+            d = defaultdict(int)
+            for c in layer:
+                d[int(c)] += 1
+            yield d
+    l = min(layers(), key = lambda l: l[0])
     print(l[1] * l[2])
