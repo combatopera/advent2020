f4cc205f7003df83a9eefe88d368154a89d5a393
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-11-19 12:14:03 +0000

    cool

--- 2019/12a.py
+++ 2019/12a.py
@@ -1,4 +1,5 @@
 from adventlib import inpath, Vector
+from itertools import islice
 import re
 
 class Moon:
@@ -7,11 +8,26 @@ class Moon:
         self.p = p
         self.v = Vector([0, 0, 0])
 
+    def attract(self, that):
+        pull = [0] * 3
+        for axis in range(3):
+            if self.p[axis] != that.p[axis]:
+                pull[axis] = (self.p[axis] < that.p[axis]) * 2 - 1
+        self.v += pull
+        that.v -= pull
+
+    def accel(self):
+        self.p += self.v
+
     def energy(self):
         return sum(map(abs, self.p)) * sum(map(abs, self.v))
 
 def main():
     moons = [Moon(Vector(map(int, re.findall('[0-9-]+', l)))) for l in inpath().read_text().splitlines()]
     for _ in range(1000):
-        pass
+        for i, m1 in enumerate(moons):
+            for m2 in islice(moons, i + 1, None):
+                m1.attract(m2)
+        for m in moons:
+            m.accel()
     print(sum(m.energy() for m in moons))
