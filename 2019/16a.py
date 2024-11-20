e71ce1de21a1afaf04d25693ba25b77354a9e448
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-11-20 23:41:19 +0000

    still works

--- 2019/16a.py
+++ 2019/16a.py
@@ -1,23 +1,27 @@
 from adventlib import inpath
 
-def _positive(outno, size):
-    i = outno - 1
-    while i < size:
-        yield range(i, min(i + outno, size))
-        i += outno * 4
+class Size:
 
-def _negative(outno, size):
-    i = 3 * outno - 1
-    while i < size:
-        yield range(i, min(i + outno, size))
-        i += outno * 4
+    def __init__(self, n):
+        self.n = n
+
+    def _ranges(self, w, i):
+        while i < self.n:
+            yield range(i, min(i + w, self.n))
+            i += w * 4
+
+    def positive(self, j):
+        return self._ranges(j + 1, j)
+
+    def negative(self, j):
+        return self._ranges(j + 1, 3 * j + 2)
 
 def main():
     def fft():
-        for outno in range(1, size + 1):
-            yield abs(sum(signal[i] for r in _positive(outno, size) for i in r) - sum(signal[i] for r in _negative(outno, size) for i in r)) % 10
+        for j in range(size.n):
+            yield abs(sum(signal[i] for r in size.positive(j) for i in r) - sum(signal[i] for r in size.negative(j) for i in r)) % 10
     signal = list(map(int, inpath().read_text().rstrip()))
-    size = len(signal)
+    size = Size(len(signal))
     for _ in range(100):
         signal = list(fft())
     print(''.join(map(str, signal[:8])))
