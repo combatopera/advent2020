c3e32b33910abb1e9f0644f381a777f52f087f90
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-11-20 22:58:18 +0000

    no longer store patterns

--- 2019/16a.py
+++ 2019/16a.py
@@ -9,15 +9,14 @@ def _pattern(i, n):
             for x in base:
                 for _ in range(1 + i):
                     yield x
-    return list(islice(g(), 1, n + 1))
+    return islice(g(), 1, n + 1)
 
 def main():
     def fft():
-        for p in patterns:
-            yield abs(sum(x * k for x, k in zip(signal, p))) % 10
+        for i in range(n):
+            yield abs(sum(x * k for x, k in zip(signal, _pattern(i, n)))) % 10
     signal = list(map(int, inpath().read_text().rstrip()))
     n = len(signal)
-    patterns = [_pattern(i, n) for i in range(n)]
     for _ in range(100):
         signal = list(fft())
     print(''.join(map(str, signal[:8])))
