88ab942f8ed76b1f3762580209e8e1b9f85b7a24
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-11-20 23:02:42 +0000

    shift on apply

--- 2019/16a.py
+++ 2019/16a.py
@@ -4,17 +4,15 @@ from itertools import islice
 base = 0, 1, 0, -1
 
 def _pattern(i):
-    def g():
-        while True:
-            for x in base:
-                for _ in range(1 + i):
-                    yield x
-    return islice(g(), 1, None)
+    while True:
+        for x in base:
+            for _ in range(1 + i):
+                yield x
 
 def main():
     def fft():
         for i in range(n):
-            yield abs(sum(x * k for x, k in zip(signal, _pattern(i)))) % 10
+            yield abs(sum(x * k for x, k in zip(signal, islice(_pattern(i), 1, None)))) % 10
     signal = list(map(int, inpath().read_text().rstrip()))
     n = len(signal)
     for _ in range(100):
