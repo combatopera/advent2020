159ca4899a210edf57f3458398ce0dfaa08f2463
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-11-21 13:08:18 +0000

    simpler

--- 2019/17a.py
+++ 2019/17a.py
@@ -8,17 +8,13 @@ def main():
     x = y = 0
     for k in Computer(map(int, inpath().read_text().split(','))):
         if 10 == k:
-            if x:
-                w = x
-                x = 0
-                y += 1
+            x = 0
+            y += 1
         else:
             grid[x, y] = chr(k)
             x += 1
-    h = y
     def g():
-        for y in range(1, h - 1):
-            for x in range(1, w - 1):
-                if all('#' == grid[Vector([x, y]) + off] for off in plus):
-                    yield x * y
+        for x, y in grid:
+            if all('#' == grid.get(Vector([x, y]) + off) for off in plus):
+                yield x * y
     print(sum(g()))
