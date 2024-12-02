6926f84ba0ec618fc85055e73e5b2ca4899defd3
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-12-02 17:00:41 +0000

    cool

---
+++ 2024/2b.py
@@ -0,0 +1,15 @@
+from adventlib import inpath
+from itertools import islice
+
+def main():
+    def safe(l):
+        def g():
+            for i, x in enumerate(v):
+                if i != remove:
+                    yield x
+        v = list(map(int, l.split()))
+        for remove in range(-1, len(v)):
+            diffs = {x - y for i, (x, y) in enumerate(zip(g(), islice(g(), 1, None)))}
+            if diffs <= {1, 2, 3} or diffs <= {-1, -2, -3}:
+                return True
+    print(sum(1 for l in inpath().read_text().splitlines() if safe(l)))
