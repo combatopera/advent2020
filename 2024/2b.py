eead1fbc7cfe9d94cb56c93a4baf49cd9ccc2f99
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-12-02 17:01:21 +0000

    redundant

--- 2024/2b.py
+++ 2024/2b.py
@@ -9,7 +9,7 @@ def main():
                     yield x
         v = list(map(int, l.split()))
         for remove in range(-1, len(v)):
-            diffs = {x - y for i, (x, y) in enumerate(zip(g(), islice(g(), 1, None)))}
+            diffs = {x - y for x, y in zip(g(), islice(g(), 1, None))}
             if diffs <= {1, 2, 3} or diffs <= {-1, -2, -3}:
                 return True
     print(sum(1 for l in inpath().read_text().splitlines() if safe(l)))
