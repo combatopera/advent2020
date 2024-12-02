fbc2f925f50e3b84491f785a7003b4c0dd656159
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-12-02 16:58:43 +0000

    refactor

--- 2024/2a.py
+++ 2024/2a.py
@@ -2,10 +2,8 @@ from adventlib import inpath
 from itertools import islice
 
 def main():
-    def safe():
-        for l in inpath().read_text().splitlines():
-            v = list(map(int, l.split()))
-            diffs = {x - y for x, y in zip(v, islice(v, 1, None))}
-            if diffs <= {1, 2, 3} or diffs <= {-1, -2, -3}:
-                yield
-    print(sum(1 for _ in safe()))
+    def safe(l):
+        v = list(map(int, l.split()))
+        diffs = {x - y for x, y in zip(v, islice(v, 1, None))}
+        return diffs <= {1, 2, 3} or diffs <= {-1, -2, -3}
+    print(sum(1 for l in inpath().read_text().splitlines() if safe(l)))
