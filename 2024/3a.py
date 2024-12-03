944c1052ce8a11ba67c5b42de07bfafb69d7d4e2
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-12-03 09:26:36 +0000

    simpler

--- 2024/3a.py
+++ 2024/3a.py
@@ -1,6 +1,5 @@
 from adventlib import inpath
-from functools import reduce
 import operator, re
 
 def main():
-    print(sum(reduce(operator.mul, map(int, t)) for t in re.findall(r'mul\((\d+),(\d+)\)', inpath().read_text())))
+    print(sum(operator.mul(*map(int, t)) for t in re.findall(r'mul\((\d+),(\d+)\)', inpath().read_text())))
