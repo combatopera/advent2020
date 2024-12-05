86743e49bc9049bdc02e70e6d84ec2f453b70feb
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-12-05 09:38:31 +0000

    yay

---
+++ 2024/5a.py
@@ -0,0 +1,20 @@
+from adventlib import inpath
+from collections import defaultdict
+from itertools import islice
+
+def enumerate2(v):
+    for j, y in enumerate(v):
+        for i, x in enumerate(islice(v, j)):
+            yield (i, j), (x, y)
+
+def main():
+    def check(u):
+        return all((y, x) not in rules for _, (x, y) in enumerate2(u))
+    ruleblock, updateblock = inpath().read_text().split('\n\n')
+    rules = {tuple(map(int, l.split('|'))) for l in ruleblock.splitlines()}
+    def g():
+        for l in updateblock.splitlines():
+            u = list(map(int, l.split(',')))
+            if check(u):
+                yield u[len(u) // 2]
+    print(sum(g()))
