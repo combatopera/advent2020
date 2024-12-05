62c38de02da16c081d57f316fd788d8e9118ef43
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-12-05 09:40:04 +0000

    inline

--- 2024/5a.py
+++ 2024/5a.py
@@ -7,13 +7,11 @@ def enumerate2(v):
             yield (i, j), (x, y)
 
 def main():
-    def check(u):
-        return all((y, x) not in rules for _, (x, y) in enumerate2(u))
     ruleblock, updateblock = inpath().read_text().split('\n\n')
     rules = {tuple(map(int, l.split('|'))) for l in ruleblock.splitlines()}
     def g():
         for l in updateblock.splitlines():
             u = list(map(int, l.split(',')))
-            if check(u):
+            if all((y, x) not in rules for _, (x, y) in enumerate2(u)):
                 yield u[len(u) // 2]
     print(sum(g()))
