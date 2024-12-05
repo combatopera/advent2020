f4c624d568c34300e5cf48c69f67ff3c383f1544
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-12-05 09:53:58 +0000

    very sneaky solution

---
+++ 2024/5b.py
@@ -0,0 +1,21 @@
+from adventlib import inpath
+from itertools import islice
+
+def enumerate2(v):
+    for j, y in enumerate(v):
+        for i, x in enumerate(islice(v, j)):
+            yield (i, j), (x, y)
+
+def main():
+    ruleblock, updateblock = inpath().read_text().split('\n\n')
+    rules = {tuple(map(int, l.split('|'))) for l in ruleblock.splitlines()}
+    def g():
+        for l in updateblock.splitlines():
+            u = list(map(int, l.split(',')))
+            if all((y, x) not in rules for _, (x, y) in enumerate2(u)):
+                continue
+            for x in u:
+                index = sum(1 for r in rules if r[0] in u and r[1] == x)
+                if index == len(u) // 2:
+                    yield x
+    print(sum(g()))
