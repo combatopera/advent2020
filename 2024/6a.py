5054a04d34dcf76b3442d09351c29da91087609d
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-12-06 17:11:56 +0000

    ren

--- 2024/6a.py
+++ 2024/6a.py
@@ -5,14 +5,14 @@ class Walker:
     def __init__(self, grid):
         self.grid = grid
 
-    def walk(self, guard, d):
+    def walk(self, guard, ring):
         while True:
-            p = guard + d[0]
+            p = guard + ring[0]
             c = self.grid.get(p)
             if c is None:
                 break
             if '#' == c:
-                d.step(1)
+                ring.step(1)
             else:
                 yield p
                 guard = p
@@ -23,5 +23,5 @@ def main():
         grid[p] = c
         if '^' == c:
             guard = p
-            d = Ring([(0, -1), (1, 0), (0, 1), (-1, 0)])
-    print(len(set(Walker(grid).walk(guard, d))))
+            ring = Ring([(0, -1), (1, 0), (0, 1), (-1, 0)])
+    print(len(set(Walker(grid).walk(guard, ring))))
