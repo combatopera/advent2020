107896c5271a4778e9e6b048fe52c74872584d02
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-12-06 16:58:45 +0000

    refactoring

--- 2024/6a.py
+++ 2024/6a.py
@@ -2,6 +2,23 @@ from adventlib import readgrid
 
 dirs = (0, -1), (1, 0), (0, 1), (-1, 0)
 
+class Walker:
+
+    def __init__(self, grid):
+        self.grid = grid
+
+    def walk(self, guard, d):
+        while True:
+            p = guard + dirs[d]
+            c = self.grid.get(p)
+            if c is None:
+                break
+            if '#' == c:
+                d = (d + 1) % len(dirs)
+            else:
+                guard = p
+                yield p
+
 def main():
     grid = {}
     for p, c in readgrid():
@@ -9,15 +26,5 @@ def main():
         if '^' == c:
             guard = p
             d = 0
-    footprint = {guard}
-    while True:
-        p = guard + dirs[d]
-        c = grid.get(p)
-        if c is None:
-            break
-        if '#' == c:
-            d = (d + 1) % len(dirs)
-        else:
-            guard = p
-            footprint.add(p)
+    footprint = set(Walker(grid).walk(guard, d))
     print(len(footprint))
