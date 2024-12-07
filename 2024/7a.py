d0dec74ff2c8bc491698b72b966bc43b716eda49
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-12-07 15:18:08 +0000

    works

---
+++ 2024/7a.py
@@ -0,0 +1,21 @@
+from adventlib import inpath
+import operator
+
+def _check(total, args):
+    for x in range(1 << (len(args) - 1)):
+        n = args[0]
+        for i, a in enumerate(args[1:]):
+            op = operator.mul if x & (1 << i) else operator.add
+            n = op(n, a)
+        if n == total:
+            return True
+
+def main():
+    def g():
+        for l in inpath().read_text().splitlines():
+            total, l = l.split(':')
+            total = int(total)
+            args = list(map(int, l.split()))
+            if _check(total, args):
+                yield total
+    print(sum(g()))
