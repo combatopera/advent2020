d759cad4175f07b4e9fc3f18f0b045f921a58dea
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-12-07 13:00:25 +0000

    finally working

---
+++ 2024/6b.py
@@ -0,0 +1,53 @@
+from adventlib import readgrid, Ring
+from itertools import islice
+
+class Walker:
+
+    def __init__(self, grid):
+        self.grid = grid
+
+    def walk(self, guard, facing):
+        ring = Ring([(0, -1), (1, 0), (0, 1), (-1, 0)]).iadd(facing)
+        while True:
+            yield guard, ring
+            p = guard + ring[0]
+            c = self.grid.get(p)
+            if c is None:
+                break
+            if '#' == c:
+                ring.iadd(1)
+            else:
+                guard = p
+
+class Obstructions:
+
+    def __init__(self, grid):
+        self.grid = grid
+
+    def _checkobstruction(self, mainfootprint, obs, guard, facing):
+        if any(p == obs for p, _ in mainfootprint):
+            return
+        footprint = mainfootprint.copy()
+        for g, r in islice(Walker({**self.grid, obs: '#'}).walk(guard, facing), 1, None):
+            if (g, r.index) in footprint:
+                print(obs, g, r.index, footprint - mainfootprint)
+                return True
+            footprint.add((g, r.index))
+
+    def walk(self, guard, facing):
+        footprint = set()
+        obstructions = set()
+        for g, r in Walker(self.grid).walk(guard, facing):
+            footprint.add((g, r.index))
+            obs = g + r[0]
+            if '.' == self.grid.get(obs) and obs not in obstructions and self._checkobstruction(footprint, obs, g, r.index):
+                obstructions.add(obs)
+        return len(obstructions)
+
+def main():
+    grid = {}
+    for p, c in readgrid():
+        grid[p] = c
+        if '^' == c:
+            guard = p
+    print(Obstructions(grid).walk(guard, 0))
