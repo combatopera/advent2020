400f2dda53c16458b8e4b6d1fbfc3604ecaeedde
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-12-07 18:14:31 +0000

    works

---
+++ 2019/21a.py
@@ -0,0 +1,34 @@
+from adventlib import inpath
+from adventlib.intcode import Computer
+import sys
+
+'''
+ABCDabcd J
+???.#### f no choice
+.??##### t no choice
+#..#?### t
+#.##?### t
+##.#??## t
+####???? f a and d may both be holes
+
+J = D and not (A and B and C)
+
+'''
+
+
+program = '''NOT A J
+NOT J T
+AND B T
+AND C T
+NOT T J
+AND D J
+WALK
+'''
+
+def main():
+    c = Computer(map(int, inpath().read_text().split(',')), list(map(ord, program)))
+    for x in c:
+        if x >= 0x80:
+            break
+        sys.stderr.write(chr(x))
+    print(x)
