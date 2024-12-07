c3dd773b62a212affb7d5466485c252ff3a49521
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-12-07 13:08:59 +0000

    simpler

--- 2024/6b.py
+++ 2024/6b.py
@@ -1,5 +1,4 @@
 from adventlib import readgrid, Ring
-from itertools import islice
 
 class Walker:
 
@@ -27,7 +26,7 @@ class Obstructions:
     def _checkobstruction(self, mainfootprint, guard, facing, obs):
         if '.' == self.grid.get(obs) and all((obs, i) not in mainfootprint for i in range(4)):
             footprint = mainfootprint.copy()
-            for g, r in islice(Walker({**self.grid, obs: '#'}).walk(guard, facing), 1, None):
+            for g, r in Walker({**self.grid, obs: '#'}).walk(guard, facing):
                 if (g, r.index) in footprint:
                     return True
                 footprint.add((g, r.index))
@@ -36,10 +35,10 @@ class Obstructions:
         footprint = set()
         obstructions = set()
         for g, r in Walker(self.grid).walk(guard, facing):
-            footprint.add((g, r.index))
             obs = g + r[0]
             if obs not in obstructions and self._checkobstruction(footprint, g, r.index, obs):
                 obstructions.add(obs)
+            footprint.add((g, r.index))
         return len(obstructions)
 
 def main():
