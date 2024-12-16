0c3d0e2bb746f58e9b9c9472d86b34316e2a6559
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-12-16 12:49:47 +0000

    still broken

--- 2024/9b.py
+++ 2024/9b.py
@@ -1,34 +1,28 @@
 from adventlib import inpath
 
-class Gap:
+class Chunk:
 
     def __init__(self, i, n):
         self.i = i
         self.n = n
 
 def main():
-    blocks = []
+    files = []
     gaps = []
+    block = 0
     for i, k in enumerate(map(int, inpath().read_text().rstrip())):
         id = None if i & 1 else i // 2
         if id is None:
-            gaps.append(Gap(len(blocks), k))
-        blocks.extend(id for _ in range(k))
-    j = len(blocks) - 1
-    while id >= 0:
-        while blocks[j] != id:
-            j -= 1
-        i = j
-        while i - 1 >= 0 and blocks[i - 1] == id:
-            i -= 1
-        n = j - i + 1
+            gaps.append(Chunk(block, k))
+        else:
+            files.append(Chunk(block, k))
+        block += k
+    for id in range(len(files))[::-1]:
+        file = files[id]
         for gap in gaps:
-            if gap.n >= n:
-                for k in range(n):
-                    blocks[gap.i + k] = id
-                    blocks[i + k] = None
-                gap.i += n
-                gap.n -= n
+            if file.n <= gap.n:
+                file.i = gap.i
+                gap.i += file.n
+                gap.n -= file.n
                 break
-        id -= 1
-    print(sum(i * id for i, id in enumerate(blocks) if id is not None))
+    print(sum(i * id for id, f in enumerate(files) for i in range(f.i, f.i + f.n)))
